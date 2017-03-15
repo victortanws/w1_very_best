087987aa9b80100c776e5e8163ab28db4d244ffe
Raghu Betina <raghu@firstdraft.com>
2017-03-15 01:18:15 +0000

    Generate cuisines resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Cuisine resource:
+  # CREATE
+  get "/cuisines/new", :controller => "cuisines", :action => "new"
+  post "/create_cuisine", :controller => "cuisines", :action => "create"
+
+  # READ
+  get "/cuisines", :controller => "cuisines", :action => "index"
+  get "/cuisines/:id", :controller => "cuisines", :action => "show"
+
+  # UPDATE
+  get "/cuisines/:id/edit", :controller => "cuisines", :action => "edit"
+  post "/update_cuisine/:id", :controller => "cuisines", :action => "update"
+
+  # DELETE
+  get "/delete_cuisine/:id", :controller => "cuisines", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
